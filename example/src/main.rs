8cf32d508c34e7e16aafa00921977ede8b4f7a6c
robbe <robinjunker04@gmail.com>
2024-04-29 22:17:38 +0200

    worked a bit on the renderer, added a time crate and coppyed an example to render form shadertoy

--- example/src/main.rs
+++ example/src/main.rs
@@ -12,10 +12,12 @@ fn main() {
 
     app.add_plugin(window::WindowPlugin);
     app.add_plugin(rendering::RenderPlugin);
-    //app.add_plugin(asset_manager::AssetManagerPlugin);
-    
-    app.schedules.add(Schedules::Update, record_deltatime_system(Instant::now()));
-    app.schedules.add(Schedules::UpdateEvery(Duration::from_secs(1)), print_delta_system());
+    app.add_plugin(time::TimePlugin);
+
+    app.schedules.add(
+        Schedules::UpdateEvery(Duration::from_secs(1)),
+        print_delta_system(),
+    );
 
     app.resources.insert(DeltaTime(0.0));
 
@@ -23,19 +25,6 @@ fn main() {
 }
 
 #[legion::system]
-fn record_deltatime(
-    #[state] last_update : &mut Instant,
-    #[resource] dt : &mut DeltaTime,
-    ) {
-    dt.0 = last_update.elapsed().as_secs_f32();
-    *last_update = Instant::now();
-}
-
-#[legion::system]
-fn print_delta(
-    #[resource] dt : &mut DeltaTime,
-    ) {
-
-    println!("fps : {}",1.0 / dt.0);
-
+fn print_delta(#[resource] time: &time::Time) {
+    println!("fps : {:.0}", 1.0 / time.delta);
 }
